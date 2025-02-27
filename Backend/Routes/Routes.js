8033b3bded706bc768602018afba941dedd6de5b
shreyasharma09 <shreya.sharma9m@gmail.com>
2025-02-28 00:43:04 +0530

    Middlewares and Category Table

--- Backend/Routes/Routes.js
+++ Backend/Routes/Routes.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const { handleResponse, handleError } = require("../Responses/Responses")
 const Users = require("../Tables/UserTable")
-const { generateotp ,verifyotp} = require("../Services/OtpService/OTPService")
+const Category = require("../Tables/CategoryTable")
+const { generateotp, verifyotp } = require("../Services/OtpService/OTPService")
 const { otptoemailforverification } = require("../Services/EmailService/EmailService")
-const jwt=require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 const Routes = express.Router()
+const { checkUserDetails } = require("../Middlewares/CheckUserDetails")
 require("dotenv").config()
 Routes.get("/", (req, resp) => handleResponse(resp, 200, "Server Health is Okay!"))
 
@@ -22,7 +24,7 @@ Routes.post("/verifyUser", async (req, resp) => {
 })
 
 Routes.post("/createUser", async (req, resp) => {
-    const { name, phone, email, password ,otp} = req.body
+    const { name, phone, email, password, otp } = req.body
     if (!name || !phone || !email || !password) return handleResponse(resp, 404, "All fields are required")
 
     const dbQuery = `Select * from ${process.env.USER_TABLE} where email='${email}'`  //find with email
@@ -41,22 +43,56 @@ Routes.post("/createUser", async (req, resp) => {
     })
 })
 
-Routes.post("/login",(req,resp)=>{
-    const {email,password}=req.body
-    if(!email || !password) return handleResponse(resp,404,"All fields are required")
+Routes.post("/login", (req, resp) => {
+    const { email, password } = req.body
+    if (!email || !password) return handleResponse(resp, 404, "All fields are required")
 
-    const dbQuery=`Select * from ${process.env.USER_TABLE} where email='${email}'`
-    Users.query(dbQuery,(error,results)=>{  //results m array aata h 
-        if(error) return handleError(resp,error)
-        if(results.length===0) return handleResponse(resp,401,"Invalid Email")
-        if(results[0].password!==password) return handleResponse(resp,401,"Invalid Password")
-        const payload={
-            id:results[0].id
+    const dbQuery = `Select * from ${process.env.USER_TABLE} where email='${email}'`
+    Users.query(dbQuery, (error, results) => {  //results m array aata h 
+        if (error) return handleError(resp, error)
+        if (results.length === 0) return handleResponse(resp, 401, "Invalid Email")
+        if (results[0].password !== password) return handleResponse(resp, 401, "Invalid Password")
+        const payload = {
+            id: results[0].id
         }
-        const token=jwt.sign(payload,process.env.JWT_KEY)
-        return handleResponse(resp,202,"Login Successfully",{token,role:results[0].role})
+        const token = jwt.sign(payload, process.env.JWT_KEY)
+        return handleResponse(resp, 202, "Login Successfully", { token, role: results[0].role })
     })
 })
+
+
+Routes.post('/createCategory', checkUserDetails, (req, resp) => {
+    const { name } = req.body;
+
+    if (!name) return handleResponse(resp, 404, "Category name is required")
+
+    // Check if category exists
+    const checkQuery = `SELECT id FROM ${process.env.CATEGORY_TABLE} WHERE name = '${name}'`;
+    Category.query(checkQuery, (error, results) => {
+        if (error) return handleError(resp, error)
+
+        if (results.length !== 0) {
+            return handleResponse(resp, 400, "This Category already exists.")
+        }
+
+        // Insert if not exists
+        const insertQuery = `INSERT INTO ${process.env.CATEGORY_TABLE} (name,user_id) VALUES (?,?)`;
+        Category.query(insertQuery, [name, req.user.id], (error, result) => {
+            if (error) return handleError(resp, error)
+
+            return handleResponse(resp, 201, "Category Created Successfully", result)
+        });
+    });
+});
+Routes.get('/getAllCategories', checkUserDetails, (req, resp) => {
+    const Query = `SELECT * FROM ${process.env.CATEGORY_TABLE} where user_id=?  ORDER BY id DESC` ;
+
+    Category.query(Query, [req.user.id], (error, results) => {
+        if (error) return handleError(resp, error)
+        return handleResponse(resp, 202, "Categories fetched successfully", results)
+    });
+});
+
 
 
 module.exports = Routes    
